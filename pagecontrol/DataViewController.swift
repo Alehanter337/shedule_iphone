9181ca26f73d1e90483202866b5c9299e25699dd
alehanter <alehanter@shmacbook.local>
2021-09-16 12:02:26 +0700

    Add shedule

--- pagecontrol/DataViewController.swift
+++ pagecontrol/DataViewController.swift
@@ -13,10 +13,26 @@ class DataViewController: UIViewController {
     var displayText: String?
     var index: Int?
     
+    @IBOutlet weak var firstLesson: UILabel!
+    var displayLesson: String?
+    
+    @IBOutlet weak var secondLesson: UILabel!
+    var displayLesson_2: String?
+    
+    @IBOutlet weak var thirdLesson: UILabel!
+    var displayLesson_3: String?
+    
+    @IBOutlet weak var FourthLesson: UILabel!
+    var displayLesson_4: String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         displayLabel.text = displayText
-        // Do any additional setup after loading the view.
+        firstLesson.text = displayLesson
+        secondLesson.text = displayLesson_2
+        thirdLesson.text = displayLesson_3
+        FourthLesson.text = displayLesson_4
     }
     
 }
